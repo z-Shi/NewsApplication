acfd8e3f975989ca7739de31ec203231f6f1ea4f
Kathleen West <2509480w@student.gla.ac.uk>
2020-04-07 21:13:45 +0100

    Minor Bug-Fix -- AJAX Method only worked once, due to dynamic nature of DOM - had to add an on-click method for the button class - rather than initial load

--- static/js/news-ajax.js
+++ static/js/news-ajax.js
@@ -1,13 +1,10 @@
-$(document).ready(function() {
-    $('.view_more_btn').click(function() {
-        var results = $(this).attr('data-results');
-        var btn = $(this)
+$(document).on('click','.view_more_btn',function () {
+    var results = $(this).attr('data-results');
+    var btn = $(this)
 
-        $.get('/news/show_more/',
-            {'results': results},
-            function(data) {
-                $('#story-list').html(data);
-                btn.hide();
-        })
+    $.get('/news/show_more/',
+        {'results': results},
+        function(data) {
+            $('#story-list').html(data);
     })
 });
