80b4dd12aad4415b8f1b71a59d2e998e5e327cb0
Kathleen West <2509480w@student.gla.ac.uk>
2020-04-07 15:35:23 +0100

    Minor Update for PythonAnywhere Hosting

--- News_Project/settings.py
+++ News_Project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'cns)w%85lx2mj=cqud=^l-^&0=ogkt%%qdy+b@_o1+lrd@c1km'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['zshi3.pythonanywhere.com', 'localhost']
 
 
 # Application definition
